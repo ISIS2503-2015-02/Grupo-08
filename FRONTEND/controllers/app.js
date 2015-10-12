19fe78621116e33b550587ee5d3962cf184e5078
Fabio <fa.espinosa10@uniandes.edu.co>
2015-10-12 18:30:26 -0500

    hola

--- FRONTEND/controllers/app.js
+++ FRONTEND/controllers/app.js
@@ -3,7 +3,7 @@
  */
 
 
-var tbc = angular.module('tbc', ['ngRoute', 'ui.bootstrap']);
+var tbc = angular.module('tbc', ['ngRoute']);
 
 
 
@@ -26,14 +26,7 @@ var tbc = angular.module('tbc', ['ngRoute', 'ui.bootstrap']);
     }])
 
 
-    tbc.directive('mainPage', function(){
-        return{
-            restrict:'A',
-            replace:true,
-            templateUrl: 'partials/main-page.html',
-            controller: 'appCtrl'
-        };
-    });
+
 
     tbc.directive('loginForm', function(){
         return{
@@ -46,17 +39,25 @@ var tbc = angular.module('tbc', ['ngRoute', 'ui.bootstrap']);
     });
 
 
-    tbc.controller('appCtrl', ['$scope', function($scope) {
-        $scope.addCompetitor=function(){
-            console.log('name');
-            $http.get('http://localhost:8080/competitors/add', JSON.stringify($scope.competitor)).success(function(data,headers){
-                $scope.competitor={};
-                $scope.toolbar.selectTab(2);
+    tbc.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.getTranvias=function(){
+            $http.get('http://exp2.diegorbaquero.com/tranvia').success(function(data,headers){
+                $scope.tranvias=data;
+
             });
         };
+        $scope.getMobibus=function(){
+            $http.get('http://exp2.diegorbaquero.com/mobibus').success(function(data,headers){
+                $scope.mobibuses=data();
+            });
+        };
+
+
+        $scope.getTranvias();
+        $scope.getMobibus;
     }]);
 
-
+//[{"id":"1","tiempoSalida":"2015-09-17 22:13:19","tiempoLlegada":"0000-00-00 00:00:00","linea":"B","longitud":"45.6","latitud":"12.3","emergencia":"1"}
 
 tbc.controller("competitorCtrl", function($http, $scope) {
 
