e96782e73eff38244dd4d8d4188c81eeee9daf7b
Fabio <fa.espinosa10@uniandes.edu.co>
2015-10-12 21:05:15 -0500

    front end

--- FRONTEND/controllers/app.js
+++ FRONTEND/controllers/app.js
@@ -40,7 +40,6 @@ var tbc = angular.module('tbc', ['ngRoute']);
 
 
     tbc.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
-
         L.mapbox.accessToken = 'pk.eyJ1IjoiZmFiaW9lc3Bpbm9zYSIsImEiOiJjaWVtMGJ4a28wMDdnNHFrbjY1OWtxbXN6In0.nSjm5Xx8l3Hg4JqTjkdm0w';
         var map = L.mapbox.map('map', 'mapbox.streets').setView([4.69, -74.085], 13);
 
@@ -52,14 +51,13 @@ var tbc = angular.module('tbc', ['ngRoute']);
                 var myLayer = L.mapbox.featureLayer().addTo(map);
                 var features = [];
                 angular.forEach($scope.tranvias, function(value, key) {
-                    console.log(value.id + " " + value.latitud + " " + value.longitud);
                     features.push({
                         type: 'Feature',
                         geometry: {
                             type: 'Point',
                             coordinates: [
-                              value.longitud,
-                              value.latitud
+                                value.longitud,
+                                value.latitud
                             ]
                         },
                         properties: {
@@ -90,8 +88,8 @@ var tbc = angular.module('tbc', ['ngRoute']);
                         geometry: {
                             type: 'Point',
                             coordinates: [
-                              value.longitud,
-                              value.latitud
+                                value.longitud,
+                                value.latitud
                             ]
                         },
                         properties: {
@@ -110,20 +108,41 @@ var tbc = angular.module('tbc', ['ngRoute']);
             });
         };
 
+        $scope.getVCubs=function(){
+            $http.get('http://exp2.diegorbaquero.com/estacion').success(function(data,headers){
+                $scope.vcubs=data;
+            });
+        };
 
+        $scope.getTranviasFuncionando=function(){
+            $http.get('http://exp2.diegorbaquero.com/tranvia/cuenta').success(function(data,header){
+                $scope.cantTranvias=data;
+            });
+        };
 
-        
+        $scope.getMobibusesFuncionando=function(){
+            $http.get('http://exp2.diegorbaquero.com/mobibus/cuenta').success(function(data,header){
+                $scope.cantMobibus=data;
+            });
+        };
 
-
-
+        $scope.getVcubsFuncionando=function(){
+            $http.get('http://exp2.diegorbaquero.com/estacion/cuenta').success(function(data,header){
+                $scope.cantVCubs=data;
+            });
+        };
 
 
         $scope.getTranvias();
         $scope.getMobibus();
+        $scope.getVCubs();
+
+        $scope.getTranviasFuncionando();
+        $scope.getMobibusesFuncionando();
+        $scope.getVcubsFuncionando();
+
+        var modWidth=800;
+        $("#map").css('width', '100%');
     }]);
 
 //[{"id":"1","tiempoSalida":"2015-09-17 22:13:19","tiempoLlegada":"0000-00-00 00:00:00","linea":"B","longitud":"45.6","latitud":"12.3","emergencia":"1"}
-
-tbc.controller("competitorCtrl", function($http, $scope) {
-
-});
