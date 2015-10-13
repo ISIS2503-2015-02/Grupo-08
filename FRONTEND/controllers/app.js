abd834ee64f2a91a6aa043f5c9851062b3202846
dscm0723 <ds.chicaiza10@uniandes.edu.co>
2015-10-12 20:08:31 -0500

    maps

--- FRONTEND/controllers/app.js
+++ FRONTEND/controllers/app.js
@@ -40,21 +40,86 @@ var tbc = angular.module('tbc', ['ngRoute']);
 
 
     tbc.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
+
+        L.mapbox.accessToken = 'pk.eyJ1IjoiZmFiaW9lc3Bpbm9zYSIsImEiOiJjaWVtMGJ4a28wMDdnNHFrbjY1OWtxbXN6In0.nSjm5Xx8l3Hg4JqTjkdm0w';
+        var map = L.mapbox.map('map', 'mapbox.streets').setView([4.69, -74.085], 13);
+
+
         $scope.getTranvias=function(){
-            $http.get('http://exp2.diegorbaquero.com/tranvia').success(function(data,headers){
+            $http.get('http://exp2.diegorbaquero.com/tranvia').success(function(data,headers)
+            {
                 $scope.tranvias=data;
+                var myLayer = L.mapbox.featureLayer().addTo(map);
+                var features = [];
+                angular.forEach($scope.tranvias, function(value, key) {
+                    console.log(value.id + " " + value.latitud + " " + value.longitud);
+                    features.push({
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [
+                              value.longitud,
+                              value.latitud
+                            ]
+                        },
+                        properties: {
+                            title: 'Tranvia ' + value.id,
+                            description: value.id + " " + value.latitud + " " + value.longitud,
+                            'marker-size': 'small',
+                            'marker-color': '#119911',
+                            'marker-symbol': 'rail-metro'
+                        }
+                    });
+                });
+                myLayer.setGeoJSON({
+                    type: 'FeatureCollection',
+                    features: features
+                })
 
             });
         };
         $scope.getMobibus=function(){
             $http.get('http://exp2.diegorbaquero.com/mobibus').success(function(data,headers){
-                $scope.mobibuses=data();
+                $scope.mobibuses=data;
+                var myLayer = L.mapbox.featureLayer().addTo(map);
+                var features = [];
+                angular.forEach($scope.mobibuses, function(value, key) {
+                    console.log(value.id + " " + value.latitud + " " + value.longitud);
+                    features.push({
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [
+                              value.longitud,
+                              value.latitud
+                            ]
+                        },
+                        properties: {
+                            title: 'Mobibus ' + value.id,
+                            description: value.id + " " + value.latitud + " " + value.longitud,
+                            'marker-size': 'small',
+                            'marker-color': '#111199',
+                            'marker-symbol': 'bus'
+                        }
+                    });
+                });
+                myLayer.setGeoJSON({
+                    type: 'FeatureCollection',
+                    features: features
+                })
             });
         };
 
 
+
+        
+
+
+
+
+
         $scope.getTranvias();
-        $scope.getMobibus;
+        $scope.getMobibus();
     }]);
 
 //[{"id":"1","tiempoSalida":"2015-09-17 22:13:19","tiempoLlegada":"0000-00-00 00:00:00","linea":"B","longitud":"45.6","latitud":"12.3","emergencia":"1"}
