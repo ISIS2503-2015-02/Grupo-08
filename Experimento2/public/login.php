3219c59f8b0ca49f05db8f30b0edf925262bf8d3
Diego Rodríguez <diegorbaquero@gmail.com>
2015-10-28 19:30:00 -0500

    Updated

--- Experimento2/public/login.php
+++ Experimento2/public/login.php
@@ -6,7 +6,7 @@
  * Time: 6:17PM
  */
 
-if($_POST["user"] =="admin" && $_POST["password"] == "p123$") {
+if($_POST["user"] =="admin" && $_POST["password"] == "123$") {
     echo "true";
 } else {
     echo "false";
