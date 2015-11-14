5a139ba90014ed7017d3484f37016d5ee4e346a4
Fabio <fa.espinosa10@uniandes.edu.co>
2015-11-14 15:18:23 -0500

    retry if response is not 200

--- FRONTEND/controllers/app.js
+++ FRONTEND/controllers/app.js
@@ -6,10 +6,6 @@
 var tbc = angular.module('tbc', ['ngRoute']);
 
 
-
-
-
-
     tbc.config(['$routeProvider', function ($routeProvider) {
 
     $routeProvider
@@ -45,87 +41,119 @@ var tbc = angular.module('tbc', ['ngRoute']);
         $scope.getTranvias=function(){
             $http.get('https://exp2.diegorbaquero.com/tranvia').success(function(data,headers)
             {
-                $scope.tranvias=data;
-                var myLayer = L.mapbox.featureLayer().addTo(map);
-                var features = [];
-                angular.forEach($scope.tranvias, function(value, key) {
-                    features.push({
-                        type: 'Feature',
-                        geometry: {
-                            type: 'Point',
-                            coordinates: [
-                                value.longitud,
-                                value.latitud
-                            ]
-                        },
-                        properties: {
-                            title: 'Tranvia ' + value.id,
-                            description: value.id + " " + value.latitud + " " + value.longitud,
-                            'marker-size': 'small',
-                            'marker-color': '#119911',
-                            'marker-symbol': 'rail-metro'
-                        }
-                    });
+                getTranviasFunction(data);
+            }).error(function(data, headers){
+                $http.get('https://exp2.diegorbaquero.com/tranvia').success(function(data,headers)
+                {
+                    getTranviasFunction(data);
                 });
-                myLayer.setGeoJSON({
-                    type: 'FeatureCollection',
-                    features: features
-                })
-
             });
         };
         $scope.getMobibus=function(){
             $http.get('https://exp2.diegorbaquero.com/mobibus').success(function(data,headers){
-                $scope.mobibuses=data;
-                var myLayer = L.mapbox.featureLayer().addTo(map);
-                var features = [];
-                angular.forEach($scope.mobibuses, function(value, key) {
-                    features.push({
-                        type: 'Feature',
-                        geometry: {
-                            type: 'Point',
-                            coordinates: [
-                                value.longitud,
-                                value.latitud
-                            ]
-                        },
-                        properties: {
-                            title: 'Mobibus ' + value.id,
-                            description: value.id + " " + value.latitud + " " + value.longitud,
-                            'marker-size': 'small',
-                            'marker-color': '#111199',
-                            'marker-symbol': 'bus'
-                        }
-                    });
+                getMobibusFunction(data);
+            }).error(function(data, headers){
+                $http.get('https://exp2.diegorbaquero.com/mobibus').success(function(data,headers){
+                    getMobibusFunction(data);
                 });
-                myLayer.setGeoJSON({
-                    type: 'FeatureCollection',
-                    features: features
-                })
             });
+        };
+
+        getTranviasFunction=function(data){
+            $scope.tranvias=data;
+            var myLayer = L.mapbox.featureLayer().addTo(map);
+            var features = [];
+            angular.forEach($scope.tranvias, function(value, key) {
+                features.push({
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [
+                            value.longitud,
+                            value.latitud
+                        ]
+                    },
+                    properties: {
+                        title: 'Tranvia ' + value.id,
+                        description: value.id + " " + value.latitud + " " + value.longitud,
+                        'marker-size': 'small',
+                        'marker-color': '#119911',
+                        'marker-symbol': 'rail-metro'
+                    }
+                });
+            });
+            myLayer.setGeoJSON({
+                type: 'FeatureCollection',
+                features: features
+            })
+        };
+
+        getMobibusFunction=function (data){
+            $scope.mobibuses=data;
+            var myLayer = L.mapbox.featureLayer().addTo(map);
+            var features = [];
+            angular.forEach($scope.mobibuses, function(value, key) {
+                features.push({
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [
+                            value.longitud,
+                            value.latitud
+                        ]
+                    },
+                    properties: {
+                        title: 'Mobibus ' + value.id,
+                        description: value.id + " " + value.latitud + " " + value.longitud,
+                        'marker-size': 'small',
+                        'marker-color': '#111199',
+                        'marker-symbol': 'bus'
+                    }
+                });
+            });
+            myLayer.setGeoJSON({
+                type: 'FeatureCollection',
+                features: features
+            })
         };
 
         $scope.getVCubs=function(){
             $http.get('https://exp2.diegorbaquero.com/estacion').success(function(data,headers){
                 $scope.vcubs=data;
+            }).error(function(data, headers){
+                $http.get('https://exp2.diegorbaquero.com/estacion').success(function(data,headers){
+                    $scope.vcubs=data;
+                });
             });
         };
 
         $scope.getTranviasFuncionando=function(){
             $http.get('https://exp2.diegorbaquero.com/tranviaCuenta').success(function(data,header){
                 $scope.cantTranvias=data;
+            }).error(function(data, headers) {
+                $http.get('https://exp2.diegorbaquero.com/tranviaCuenta').success(function (data, headers) {
+                    $scope.vcubs = data;
+                });
             });
         };
 
         $scope.getMobibusesFuncionando=function(){
             $http.get('https://exp2.diegorbaquero.com/mobibusCuenta').success(function(data,header){
                 $scope.cantMobibus=data;
-            });
+            }).error(function(data, headers){
+                $http.get('https://exp2.diegorbaquero.com/mobibusCuenta').success(function(data,header){
+                    $scope.cantMobibus=data;
+                });
+            })
         };
 
         $scope.getVcubsFuncionando=function(){
             $http.get('https://exp2.diegorbaquero.com/estacionCuenta').success(function(data,header){
                 $scope.cantVCubs=data;
+            }).error(function(data, headers) {
+                $http.get('https://exp2.diegorbaquero.com/estacionCuenta').success(function (data, headers) {
+                    $scope.vcubs = data;
+                });
             });
         };
 
@@ -137,17 +165,21 @@ var tbc = angular.module('tbc', ['ngRoute']);
             console.log($scope.user);
             $http.get('https://exp2.diegorbaquero.com/login/'+$scope.user.user +'/'+$scope.user.password).success(function(data,headers)
             {
-                console.log(data);
-                if(data==="false"){
-                    alert("Incorrecto");
-                    console.log("jeje");
-                    //botar error
-                }
-                else{
-                    $scope.logged=true;
-                    console.log("sis");
-                }
-            });
+                ingresarFunction(data);
+            }).error(function(data, headers){
+                ingresarFunction(data);
+            })
+        };
+
+        ingresarFunction=function(data){
+            console.log(data);
+            if(data==="false"){
+                alert("Incorrecto");
+            }
+            else{
+                $scope.logged=true;
+                console.log("sis");
+            }
         };
 
 
